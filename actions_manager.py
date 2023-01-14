8e45d8682e8d687c719934d9854371e418df2c9b
dankrecoum <dankrecoum@gmail.com>
2023-01-14 11:32:24 -0500

    update find position

--- actions_manager.py
+++ actions_manager.py
@@ -1,12 +1,20 @@
-from dataclasses import dataclass
-from typing import List, Dict
-
 from actions import BuildAction, SellAction, SendReinforcementsAction
+from dataclasses import dataclass
+from dataclasses import dataclass
+from game_message import *
 from game_message import Map, Shop, TowerType, Position, EnemyType
+from typing import List, Dict
+from typing import List, Dict
 
 
 def find_best_position():
-    return Position(0, 0)
+    return Position(0,0)
+
+# def find_starter_position_for_tower(self):
+#     # # return surrounding position
+#     # path_start = self.starter_position
+#     # surround:Position[] = [path_start]
+#     return self.starter_position.x
 
 
 def find_best_enemy_type():
@@ -14,12 +22,13 @@ def find_best_enemy_type():
 
 
 class ActionManager:
-    def __init__(self, _map):
-        self._map = _map
-        self.path = []
+    def __init__(self, game_message: GameMessage):
+        self.game_message: GameMessage = game_message
+        self.paths = list()
+        self.starter_position: Position = game_message.map.paths[0].tiles[0]
         self.actions_queue = list()
 
-    def add_tour(self):
+    def add_tower(self):
         position: Position = find_best_position()
         self.actions_queue.append(BuildAction(TowerType.SPEAR_SHOOTER, position))
 
@@ -30,4 +39,3 @@ class ActionManager:
     def send_reinforcement(self, other_teams_id):
         reinforcement_enemy_type: EnemyType = find_best_enemy_type()
         self.actions_queue.append(SendReinforcementsAction(reinforcement_enemy_type, other_teams_id))
-
--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ class Bot:
         """
 
         other_team_ids = [team for team in game_message.teams if team != game_message.teamId]
-        actions_manager = ActionManager(game_message.map)
-        actions_manager.sell_action()
-        actions_manager.add_tour()
+        actions_manager = ActionManager(game_message)
+        # actions_manager.sell_action()
+        actions_manager.add_tower()
 
         if other_team_ids:
             actions_manager.send_reinforcement(other_team_ids[0])
